c4146dbe642cce7102164543a553dbff36e0f291
Thomas Dimnet <thomas.dimnet@gmail.com>
2019-09-30 19:54:37 +0200

    Code review du 30 Septembre.

---
+++ app.js
@@ -0,0 +1,25 @@
+const numberOfCases = 10;
+
+const $row = document.querySelectorAll(".row");
+
+/**
+ * TODO NEXT WEEK :
+ * - Tranduire de Vanilla JavaScript en jQuery
+ * - Réaliser le tableau avec x et y.
+ * - Sélectionner une case via jQuery et la rendre grisée
+ */
+
+// console.log(row)
+
+// for (let i = 1; i <= numberOfCases; i++) {
+//   // console.log(i)
+//   const $case = document.createElement("div");
+
+//   // Première notation possible
+// //   $case.classList = "case x-" + i ;
+
+//   // Deuxième notation possible
+//   $case.classList = `case x-${i}`;
+
+//   $row[0].appendChild($case);
+// }
