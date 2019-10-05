e1f1da6d255b1f7b8a31c693045b48ee7f244e2e
Yannick <guyader.yannick@gmail.com>
2019-10-05 15:58:59 +0200

    ajout class axe des ordonnées

--- app.js
+++ app.js
@@ -8,7 +8,7 @@ for (let j = 1; j <= numberOflines; j++) {
     const $row = $('<div class="row"></div>');
     for (let i = 1; i <= numberOfCases; i++) {
         const $case = document.createElement("div");
-        $case.className = `case x-${i}`;
+        $case.className = `case x-${i} y-${j}`;
         $row[0].appendChild($case);
     }
     $game.append($row);
