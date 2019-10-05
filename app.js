6d689c818c21987d4bc6bd537a29cd60bb4fbfda
Yannick <guyader.yannick@gmail.com>
2019-10-05 15:25:58 +0200

    génération de la board

--- app.js
+++ app.js
@@ -1,43 +1,15 @@
 const numberOfCases = 10;
 const numberOflines = 10;
 
-const $row = $('div.row');
+const $game = $('.game');
 
 
-for (let j = 1; j < numberOflines; j++) {
-
-    for (let i = 0; i < numberOfCases; i++) {
+for (let j = 1; j <= numberOflines; j++) {
+    const $row = $('<div class="row"></div>');
+    for (let i = 1; i <= numberOfCases; i++) {
         const $case = document.createElement("div");
         $case.className = `case x-${i}`;
         $row[0].appendChild($case);
     }
-    const $line = document.createElement("div");
-    $line.className = "row";
-    $row[0].after($line);
+    $game.append($row);
 }
-
-/*
- * TODO NEXT WEEK :
- * - Traduire de Vanilla JavaScript en jQuery OK
- * - Réaliser le tableau avec x et y.
- * - Sélectionner une case via jQuery et la rendre grisée
- */
-
-
-// for (let i = 1; i <= numberOfCases; i++) {
-//     //const $case = document.createElement("div");
-//     console.log(i)
-//     const $case = $(".row").after( "<div class='row'>" );
-//     $case.classList = "x-" + i ;
-
-//     $case.classList = `case x-${i}`;
-// }
-
-// for (let j = 1; j <= numberOflines; j++) {
-//     const $case = document.createElement("div");
-//     $(".row").after( "<div class='row'>" )
-//     $case.classList = `case x-${i}`;
-//     $row[0].appendChild($case);
-// }
-
-
