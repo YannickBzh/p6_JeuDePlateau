0ec260838c1e0467a402e401c8aae29cc94de055
Yannick <guyader.yannick@gmail.com>
2019-10-10 21:50:30 +0200

    Add players

--- app.js
+++ app.js
@@ -1,6 +1,8 @@
 const numberOfCases = 10;
 const numberOflines = 10;
+const numberOfCaseGrey = 10;
 
+// $game = Noeud HTML (Html Element)
 const $game = $('.game');
 
 
@@ -8,16 +10,52 @@ for (let j = 1; j <= numberOflines; j++) {
     const $row = $('<div class="row"></div>');
     for (let i = 1; i <= numberOfCases; i++) {
         const $case = document.createElement("div");
-        $case.className = `case x-${i} y-${j}`;
+        $case.className = `case x-${i} y-${j} empty`;
         $row[0].appendChild($case);
     }
     $game.append($row);
 }
 
-const $selectAllCases = $('.case');
+// $selectAllCases = Noeud HTML (Html Element)
+// const $selectAllCases = $('.empty');
 
-for (let i = 1 ; i <= 10 ; i++) {
-    let randomNumber = Math.floor((Math.random()*99)+1);
+// console.log($selectAllCases)
+
+
+for (let i = 1; i <= numberOfCaseGrey; i++) {
+    // Selection toutes mes cases empty
+    const $selectAllCases = $('.empty');
+
+    // Crée un indice random par rapport à toutes mes classes empty
+    let randomNumber = Math.floor(( Math.random() * $selectAllCases.length - 1 )+1);
+
+    // Ajoute lui la classe caseGrey
     $selectAllCases[randomNumber].classList.add("caseGrey");
-    console.log(randomNumber)
+    
+    // Supprime la classe
+    $selectAllCases[randomNumber].classList.remove("empty");
 }
+
+for (let i = 1; i <= 1; i++) {
+    const $selectAllCases = $('.empty');
+    let tedPlayer =  Math.floor(( Math.random() * $selectAllCases.length - 1 )+1);
+    $selectAllCases[tedPlayer].classList.add("tedPlayer");
+    $selectAllCases[tedPlayer].classList.remove("empty");
+}
+
+for (let i = 1; i <= 1; i++) {
+    const $selectAllCases = $('.empty');
+    let tedPlayer =  Math.floor(( Math.random() * $selectAllCases.length - 1 )+1);
+    $selectAllCases[tedPlayer].classList.add("barneyPlayer");
+    $selectAllCases[tedPlayer].classList.remove("empty");
+}
+
+
+/**
+ * TODO : 
+ *  - Maintenant que tu sais comment positionner les greyCases, tu peux faire pareil avec les weapons et les players.
+ *  - Gestion du déplacement (c.f. dossier déplacement)
+ *      -> l'idée est pour l'instant quand on clique sur une box que le player se déplace
+ *      -> on verra après pour gérer les cases adjacentes.
+ */
+
