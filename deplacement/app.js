fd8ef49deede3862bf4a00eb95787faddc526524
Yannick <guyader.yannick@gmail.com>
2019-10-24 19:48:38 +0200

    indentation ok

--- deplacement/app.js
+++ deplacement/app.js
@@ -1,21 +1,36 @@
+// Fonction de factorisation
+// function getPosition(_name, _position, _abs, _ord) {
+//     _name = _position.attr('class'); // je sors les classes
+//     _abs = _name.substring(7, 9); // je sors l'abscisse
+//     _ord = _name.substring(11, 13) // je sors l'ordonnée
+// }
+
+
+
 $('.case').click(function () {
     if ($(this).hasClass('empty')) {
         let newClass = $(this).attr('class'); // position de la case cliquée
         let newAbscisse = newClass.substring(7, 9); // je sors l'abscisse de la case cliquée
         let newOrdonnee = newClass.substring(11, 13); // je sors l'ordonnée de la case cliquée
         let playerPosition = $('.player'); // Position du player
+        // getPosition(playerClass, playerPosition, playerAbscisse, playerOrdonnee)
         let playerClass = playerPosition.attr('class'); // je sors les classes du player
         let playerAbscisse = playerClass.substring(7, 9); // je sors l'abscisse du player
         let playerOrdonnee = playerClass.substring(11, 13); // je sors l'ordonnée du player
         for (let i = 1; i <= 2; i++) {
-            if ((parseInt(newAbscisse) == parseInt(playerAbscisse) + i) && (parseInt(newOrdonnee) == parseInt(playerOrdonnee)) || ((parseInt(newOrdonnee) == parseInt(playerOrdonnee) + i) && (parseInt(newAbscisse) == parseInt(playerAbscisse))) || ((parseInt(newAbscisse) == parseInt(playerAbscisse) - i) && (parseInt(newOrdonnee) == parseInt(playerOrdonnee)) || ((parseInt(newOrdonnee) == parseInt(playerOrdonnee) - i) && (parseInt(newAbscisse) == parseInt(playerAbscisse))))) {
+            if ((newAbscisse == parseInt(playerAbscisse) + i) && (newOrdonnee == parseInt(playerOrdonnee)) || 
+            (newOrdonnee == parseInt(playerOrdonnee) + i) && (newAbscisse == parseInt(playerAbscisse)) || 
+            (newAbscisse == parseInt(playerAbscisse) - i) && (newOrdonnee == parseInt(playerOrdonnee)) || 
+            (newOrdonnee == parseInt(playerOrdonnee) - i) && (newAbscisse == parseInt(playerAbscisse))) {
                 $(this).removeClass('empty');
                 let bluePlayer = $('.player');
                 bluePlayer.removeClass("player");
                 bluePlayer.addClass("empty");
                 $(this).addClass("player");
             }
-            else { console.log('faux') }
+            //else {
+              //  console.log('faux')
+            //}
         }
     }
 })
