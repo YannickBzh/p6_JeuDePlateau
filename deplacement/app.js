5a0b81b9ae9169d645c2bf8beabe77261aaf4d28
Yannick <guyader.yannick@gmail.com>
2019-10-24 06:52:26 +0200

    Ajout boucle For => déplacement sur 2 cases fonctionnel

--- deplacement/app.js
+++ deplacement/app.js
@@ -1,33 +1,22 @@
-//let playerPosition = $('.player'); // je cherche la position sur laquelle se situe le joueur
-//console.log(playerPosition.attr('class')) // je sors ses classes
-//console.log(playerPosition.attr('class')[7]) // Je sors son abscisse
-//console.log(playerPosition.attr('class')[11]) // Je sors son ordonnée
-
-let playerPosition = $('.player'); // Position du player
-let playerClass = playerPosition.attr('class'); // je sors les classes du player
-
 $('.case').click(function () {
     if ($(this).hasClass('empty')) {
         let newClass = $(this).attr('class'); // position de la case cliquée
-        console.log(newClass)
         let newAbscisse = newClass.substring(7, 9); // je sors l'abscisse de la case cliquée
         let newOrdonnee = newClass.substring(11, 13); // je sors l'ordonnée de la case cliquée
         let playerPosition = $('.player'); // Position du player
         let playerClass = playerPosition.attr('class'); // je sors les classes du player
         let playerAbscisse = playerClass.substring(7, 9); // je sors l'abscisse du player
         let playerOrdonnee = playerClass.substring(11, 13); // je sors l'ordonnée du player
-        if ((parseInt(newAbscisse) == parseInt(playerAbscisse) + 1) && (parseInt(newOrdonnee) == parseInt(playerOrdonnee)) || ((parseInt(newOrdonnee) == parseInt(playerOrdonnee) + 1) && (parseInt(newAbscisse) == parseInt(playerAbscisse))) || ((parseInt(newAbscisse) == parseInt(playerAbscisse) - 1) && (parseInt(newOrdonnee) == parseInt(playerOrdonnee)) || ((parseInt(newOrdonnee) == parseInt(playerOrdonnee) - 1) && (parseInt(newAbscisse) == parseInt(playerAbscisse))))) {
-            $(this).removeClass('empty');
-            let bluePlayer = $('.player');
-            bluePlayer.removeClass("player");
-            bluePlayer.addClass("empty");
-            $(this).addClass("player");
+        for (let i = 1; i <= 2; i++) {
+            if ((parseInt(newAbscisse) == parseInt(playerAbscisse) + i) && (parseInt(newOrdonnee) == parseInt(playerOrdonnee)) || ((parseInt(newOrdonnee) == parseInt(playerOrdonnee) + i) && (parseInt(newAbscisse) == parseInt(playerAbscisse))) || ((parseInt(newAbscisse) == parseInt(playerAbscisse) - i) && (parseInt(newOrdonnee) == parseInt(playerOrdonnee)) || ((parseInt(newOrdonnee) == parseInt(playerOrdonnee) - i) && (parseInt(newAbscisse) == parseInt(playerAbscisse))))) {
+                $(this).removeClass('empty');
+                let bluePlayer = $('.player');
+                bluePlayer.removeClass("player");
+                bluePlayer.addClass("empty");
+                $(this).addClass("player");
+            }
+            else { console.log('faux') }
         }
-        else { console.log('faux') }
-        // console.log('====')
-        // console.log($(this).attr('class'))
-        // console.log($(this).attr('class')[7])
-        // console.log($(this).attr('class')[11])
     }
 })
 
