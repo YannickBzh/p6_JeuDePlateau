c88d4e0ac025351e040b622b2bb7939210c55760
tdimnetsource <thomas.dimnet@gmail.com>
2019-12-07 10:48:49 +0100

    Fix typo issue

--- game-oop/weapons.js
+++ game-oop/weapons.js
@@ -6,6 +6,6 @@
 class Weapons extends GameAttribute {
     constructor(name, damage, className) {
         super(name, className);
-        this._className = className;
+        this.damage = damage;
     }
 }
