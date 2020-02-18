cddc97ad5a74d9100c686f0475c7a35ba5ed9581
YannickBzh <guyader.yannick@gmail.com>
2020-02-18 14:31:13 +0100

    Test refacto bis

--- game-oop/game.js
+++ game-oop/game.js
@@ -246,10 +246,10 @@ class Game {
         for (let i = 1; i <= 1; i++) {
             let bottomCaseOrdonnee = (parseInt(positionPlayers[1]) + i);
             for (let j = 0; j < positionPlayers[2].length; j++) {
-                let casesAbs = positionPlayers[2][j].classList[1];
-                let casesOrd = positionPlayers[2][j].classList[2];
-                let casesAbscisse = casesAbs.substring(2, 4);
-                let casesOrdonnee = casesOrd.substring(2, 4);
+                let casesAbs = positionPlayers[2][j].classList[1];// je sors la classe des abscisses de cases
+                let casesOrd = positionPlayers[2][j].classList[2];// je sors la classe des ordonnées de cases
+                let casesAbscisse = casesAbs.substring(2, 4);// j'isole le nombre des abscisses de mes cases
+                let casesOrdonnee = casesOrd.substring(2, 4);// J'isole le nombre des ordonnées de mes cases
                 if ((casesOrdonnee == bottomCaseOrdonnee) && (casesAbscisse == parseInt(positionPlayers[0]))) {
                     if (positionPlayers[2][j].classList.contains('player-2')) {
                         this.eraseHighlight();
@@ -266,10 +266,10 @@ class Game {
         for (let i = 1; i <= 1; i++) {
             let leftCaseAbscisse = (parseInt(positionPlayers[0]) - i);
             for (let j = 0; j < positionPlayers[2].length; j++) {
-                let casesAbs = positionPlayers[2][j].classList[1];
-                let casesOrd = positionPlayers[2][j].classList[2];
-                let casesAbscisse = casesAbs.substring(2, 4);
-                let casesOrdonnee = casesOrd.substring(2, 4);
+                let casesAbs = positionPlayers[2][j].classList[1];// je sors la classe des abscisses de cases
+                let casesOrd = positionPlayers[2][j].classList[2];// je sors la classe des ordonnées de cases
+                let casesAbscisse = casesAbs.substring(2, 4);// j'isole le nombre des abscisses de mes cases
+                let casesOrdonnee = casesOrd.substring(2, 4);// J'isole le nombre des ordonnées de mes cases
                 if ((casesAbscisse == leftCaseAbscisse) && (casesOrdonnee == parseInt(positionPlayers[1]))) {
                     if (positionPlayers[2][j].classList.contains('player-2')) {
                         this.eraseHighlight();
